d5c19c07954defe75d3e1dc194e5f86e4f41dc9a
TheCrazyBeaver <47144753+TheCrazyBeaver@users.noreply.github.com>
2019-02-16 12:49:17 +0100

    Initial commit

---
+++ aosp_star2lte.mk
@@ -0,0 +1,10 @@
+# Copyright (C) 2018 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/telephony.mk)
+
+PRODUCT_NAME := aosp_star2lte
